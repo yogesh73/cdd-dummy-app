ad7ad610ed45119d99a19f297ecef0dff46f9d2b
Tehila <tehila.menachem@broadcom.com>
2019-01-14 09:37:31 +0200

    Fix Demo App exception

--- src/main/java/com/ca/cdd/dummy/lib/InnerClassCallToClass.java
+++ src/main/java/com/ca/cdd/dummy/lib/InnerClassCallToClass.java
@@ -3,27 +3,35 @@ package com.ca.cdd.dummy.lib;
 public class InnerClassCallToClass extends Library {
 
     private CallPrintStr callPrintStr;
+    private static String NAME = "InnerClassCallToClass";
 
     public InnerClassCallToClass(CallPrintStr callPrintStr) {
         this.callPrintStr = callPrintStr;
     }
 
-
-
-    public void print(String s) {
-        callPrintStr.innerClass(s);
+    public InnerClassCallToClass() {
     }
-
-    private static String NAME = new CallPrintStr().innerClass("CallPrintStr");
 
     @Override
     public String getName() {
         return NAME;
     }
 
-    public static class CallPrintStr {
+    public void print(String s) {
+        callPrintStr.innerClass(s);
+    }
 
-         public String innerClass (String s) {
+    public static class CallPrintStr extends Library{
+
+        private static String NAME = "CallPrintStr";
+
+        @Override
+        public String getName() {
+            return NAME;
+        }
+
+
+        public String innerClass (String s) {
              System.out.println(s);
              return s;
          }
