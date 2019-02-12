f55a7f4bee60ebda45f17f781f568e42106013c8
nir benaharon <nir.benaharon@broadcom.com>
2019-02-12 14:13:01 +0200

    add more Multi Threads

--- src/main/java/com/ca/cdd/dummy/lib/AnonymousClass.java
+++ src/main/java/com/ca/cdd/dummy/lib/AnonymousClass.java
@@ -14,6 +14,7 @@ public class AnonymousClass extends Library implements InterfaceClass {
         }
     };
 
+
     @Override
     public Integer useAbstractInInterface() {
         return anonymusMethos.useAbstractInInterface();
