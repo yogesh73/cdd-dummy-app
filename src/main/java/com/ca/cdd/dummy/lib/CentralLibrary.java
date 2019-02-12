a119c298db4e92f19431e7931f273697188003a8
nir benaharon <nir.benaharon@broadcom.com>
2019-02-12 13:19:54 +0200

    add more Multi Threads

--- src/main/java/com/ca/cdd/dummy/lib/CentralLibrary.java
+++ src/main/java/com/ca/cdd/dummy/lib/CentralLibrary.java
@@ -1,11 +1,16 @@
 package com.ca.cdd.dummy.lib;
 
 public class CentralLibrary extends Library {
- 
+
     private static String NAME = "Central";
  
     @Override
-    public String getName() {
+    public String getName()  {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return NAME;
     }
 }
