eeeb666f2ad9e2c198c4e4dffcbdc9f2b840e421
Nir Ben Aharon <nir.benaharon@broadcom.com>
2019-02-12 11:41:25 +0200

    Update CentralLibrary.java

--- src/main/java/com/ca/cdd/dummy/lib/CentralLibrary.java
+++ src/main/java/com/ca/cdd/dummy/lib/CentralLibrary.java
@@ -2,9 +2,7 @@ package com.ca.cdd.dummy.lib;
 
 public class CentralLibrary extends Library {
     private static String NAME = "Central";
-    
-    
-
+ 
     @Override
     public String getName() {
         return NAME;
