cf3f1ef61f9fe61717eafbfcefe3f074c2830c2e
nir benaharon <nir.benaharon@broadcom.com>
2019-02-14 10:56:00 +0200

    add more Multi Threads

--- src/main/java/com/ca/cdd/dummy/lib/ComicsLibrary.java
+++ src/main/java/com/ca/cdd/dummy/lib/ComicsLibrary.java
@@ -3,8 +3,6 @@ package com.ca.cdd.dummy.lib;
 public class ComicsLibrary extends Library {
     private static String NAME = "Comics";
 
-
-
     @Override
     public String getName() {
         return NAME;
