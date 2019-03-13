98415679338212d0141207383c9fd3bb26b53840
Nir Ben Aharon <nir.benaharon@broadcom.com>
2019-03-13 09:15:41 +0200

    sdffs

--- src/main/java/com/ca/cdd/dummy/lib/FailTestSuite.java
+++ src/main/java/com/ca/cdd/dummy/lib/FailTestSuite.java
@@ -3,6 +3,8 @@ package com.ca.cdd.dummy.lib;
 public class FailTestSuite extends Library {
     private static String NAME = "FailScenario";
 
+
+
     @Override
     public String getName() {
         return NAME;
