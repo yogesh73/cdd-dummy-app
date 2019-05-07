28553f1409944e82e6e17a7d433c8b7d6cef0bb3
Allan Reiffman <allan.reiffman@broadcom.com>
2019-05-07 11:13:21 +0300

    Update AnonymousClass.java

--- src/main/java/com/ca/cdd/dummy/lib/AnonymousClass.java
+++ src/main/java/com/ca/cdd/dummy/lib/AnonymousClass.java
@@ -6,6 +6,9 @@ public class AnonymousClass extends Library implements InterfaceClass {
    
    
    
+   
+   
+   
   InterfaceClass anonymusMethos = new InterfaceClass() {
         @Override
         public Integer useAbstractInInterface() {
