a74639eaa3e5198e9a8d7070a52d5f3390496aff
Bldcddbuild Co <bldcddbuild.co@broadcom.com>
2019-09-08 12:51:19 +0300

    From cdd-integration-tests, by root@ratesting-l28873.lvn.broadcom.net on (Sun Sep 08 09:51:19 GMT 2019)

--- src/test/java/startSuite/AnonymousClassType.java
+++ src/test/java/startSuite/AnonymousClassType.java
@@ -22,5 +22,3 @@ public class AnonymousClassType extends LocalTestConfig{
 // Recently Modified tests suites App Wc2cC
 // Recently Modified tests suites App v1Yez
 // Recently Modified tests suites App SvRmq
-
-// Recently Modified tests suites App VjNwj
