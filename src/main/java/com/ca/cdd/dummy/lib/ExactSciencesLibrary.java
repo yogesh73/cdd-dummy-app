6bc3d2fa644a11a8a084ca5c6c7a99d9c9cb0618
Ofer Yaniv <ofer.yaniv@broadcom.com>
2019-10-31 22:56:52 +0200

    Update ExactSciencesLibrary.java

--- src/main/java/com/ca/cdd/dummy/lib/ExactSciencesLibrary.java
+++ src/main/java/com/ca/cdd/dummy/lib/ExactSciencesLibrary.java
@@ -2,8 +2,7 @@ package com.ca.cdd.dummy.lib;
 
 public class ExactSciencesLibrary extends Library {
     private static String NAME = "ExactSciences";
-    
-    
+        
     @Override
     public String getName() {
         return NAME;
