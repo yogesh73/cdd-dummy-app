9e5217f305bdc01188c7ca9e7f6b791610d8d075
Nir Ben Aharon <nir.benaharon@broadcom.com>
2019-12-22 10:56:02 +0200

    US542878

--- src/main/java/com/ca/cdd/dummy/lib/AnonymousClass.java
+++ src/main/java/com/ca/cdd/dummy/lib/AnonymousClass.java
@@ -49,23 +49,3 @@ public class AnonymousClass extends Library implements InterfaceClass {
 // DummyApp
 // fa36dc3d-f2e9-4cce-9ee8-f593880b8677
 
-// DummyApp
-// b0594d36-2ae6-4a41-8af9-54cf9a13d072
-// c7149c9b-5c20-4f59-b026-3d711dad260a
-// // added from cdd-integration-tests/E2eWorkItemsInReleaseQualityReport.java
-// DummyApp
-// b2051ef6-620c-412d-a798-79c4f8374d10
-// DummyApp23232323
-// DummyApp2323
-// DummyApp
-// 0b5a1f1f-8485-4ca6-b782-222be12dd4a4
-// 841c3699-d250-466c-9e81-12d99cb508c4
-// d91434e3-346a-41b0-b656-2bbef424e3f9
-// 1c04f413-cc25-4f74-8f9a-f0c137c024a6
-// DummyApp
-// a07cd497-e8f4-49f8-8a2a-db57880f69b1
-// edf318a8-1aa0-40bd-a7c4-d3ea2924f201
-// DummyApp
-// DummyApp
-// DummyApp
-// DummyApp2323232323232323
