2a681bc733c7d5a5326b6936a9343bf84bb0fd2f
Nir Ben Aharon <nir.benaharon@broadcom.com>
2019-12-22 12:48:47 +0200

    US542878 - test
    US542878

--- src/main/java/com/ca/cdd/dummy/lib/AnonymousClass.java
+++ src/main/java/com/ca/cdd/dummy/lib/AnonymousClass.java
@@ -34,18 +34,5 @@ public class AnonymousClass extends Library implements InterfaceClass {
         return NAME;
     }
 }
-// class2223333
 
-// classaas222saas
-
-// 57aFna
-
-// FBsVj
-// UvuwQ
-// d33113ff-0c80-47cf-9a1f-1c34fb121309
-// 068758ff-a894-4620-a1a5-7ad46f615072
-// b178d309-b91a-45d6-a6dc-74e2e6ee11c1
-// 6c8a2ecd-792e-4bca-9f18-5da62d853882
-// DummyApp
-// fa36dc3d-f2e9-4cce-9ee8-f593880b8677
 
