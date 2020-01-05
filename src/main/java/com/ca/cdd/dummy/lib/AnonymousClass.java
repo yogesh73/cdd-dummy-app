33586ce9c5e20dd78e6643c40eebf6496320843e
Yochay Menachem <yochay.menachem@broadcom.com>
2020-01-05 13:29:54 +0200

    Update AnonymousClass.java

--- src/main/java/com/ca/cdd/dummy/lib/AnonymousClass.java
+++ src/main/java/com/ca/cdd/dummy/lib/AnonymousClass.java
@@ -4,6 +4,8 @@ public class AnonymousClass extends Library implements InterfaceClass {
  
   private static String NAME = "AnonymousClass";
  
+ 
+ 
   InterfaceClass anonymusMethos = new InterfaceClass() {
         @Override
         public Integer useAbstractInInterface() {
@@ -32,26 +34,3 @@ public class AnonymousClass extends Library implements InterfaceClass {
         return NAME;
     }
 }
-
-// b139abc52
-
-// DummyApp
-
-// DummyApp
-// DummyApp
-// DummyApp
-// DummyApp
-// DummyApp
-// DummyApp
-// DummyApp
-// DummyApp
-// DummyApp
-// DummyApp
-// DummyApp
-// DummyApp
-// DummyApp
-// DummyApp
-// DummyApp
-// DummyApp
-// 13fdcef2-55c5-47cd-a3b7-6e809af3f107
-// DummyApp
