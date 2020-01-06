f7af3a7d3d56961ff4514e9e9adc6b565e076aba
Bldcddbuild Co <bldcddbuild.co@broadcom.com>
2020-01-06 09:29:04 +0200

    US542878

--- src/main/java/com/ca/cdd/dummy/lib/AnonymousClass.java
+++ src/main/java/com/ca/cdd/dummy/lib/AnonymousClass.java
@@ -38,3 +38,4 @@ public class AnonymousClass extends Library implements InterfaceClass {
 // OnlineBanking
 // Xzz07
 // DummyApp
+// added from cdd-integration-tests/E2eWorkItemsInReleaseQualityReport.java
