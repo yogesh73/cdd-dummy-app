a090e01b2def8ac20b7ec947ce490b803232c47d
Bldcddbuild Co <bldcddbuild.co@broadcom.com>
2020-02-09 22:34:00 +0200

    From cdd-integration-tests, by jenkins@cdd-build-agent-15.lvn.broadcom.net on (Sun Feb 09 15:33:59 EST 2020)

--- src/main/java/com/ca/cdd/dummy/lib/AnonymousClass.java
+++ src/main/java/com/ca/cdd/dummy/lib/AnonymousClass.java
@@ -45,3 +45,4 @@ public class AnonymousClass extends Library implements InterfaceClass {
 // dc3b3ea4-4e84-448c-94d9-297aac48d60a
 // 47317216-59b9-4427-b786-9ad1a1e2d424
 // dcfc0380-efe6-4456-8a9a-dc526300b12f
+// def659f2-34da-4954-96cd-edac6375850a
