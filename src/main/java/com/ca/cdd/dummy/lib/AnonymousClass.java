a2db525b25921b7930848d8dbe5d0f5c137bcabf
Yonatan Argov <yonatan.argov@broadcom.com>
2020-02-12 10:24:47 +0200

    Update AnonymousClass.java

--- src/main/java/com/ca/cdd/dummy/lib/AnonymousClass.java
+++ src/main/java/com/ca/cdd/dummy/lib/AnonymousClass.java
@@ -38,13 +38,4 @@ public class AnonymousClass extends Library implements InterfaceClass {
 
 
 
-// DummyApp
-// DummyApp
-// 64df087b-baa2-4ad4-abf9-859c4c36ab5e
-// b0f973cb-465d-486d-9cda-f73ff763b486
-// dc3b3ea4-4e84-448c-94d9-297aac48d60a
-// 47317216-59b9-4427-b786-9ad1a1e2d424
-// dcfc0380-efe6-4456-8a9a-dc526300b12f
-// def659f2-34da-4954-96cd-edac6375850a
-// DummyApp
-// DummyApp
+// DummyApp 7.2
