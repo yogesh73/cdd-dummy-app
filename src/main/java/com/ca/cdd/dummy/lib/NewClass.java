7db9941a01e076996ff27bff2d8859211f616732
Yonatan Argov <yonatan.argov@broadcom.com>
2020-05-10 18:14:58 +0300

    new temp

---
+++ src/main/java/com/ca/cdd/dummy/lib/NewClass.java
@@ -0,0 +1,35 @@
+package com.ca.cdd.dummy.lib;
+
+public class NewClass extends Library implements InterfaceClass {
+
+  
+  
+  private static String NAME = "NewClass";
+  InterfaceClass anonymusMethos = new InterfaceClass() {
+        @Override
+        public Integer useAbstractInInterface() {
+            return null;
+         
+        }
+   
+        @Override
+        public String interfaceStingMethod() {
+            return "Returned From Anonymous Class ";
+        }
+    };
+
+    @Override
+    public Integer useAbstractInInterface() {
+        return anonymusMethos.useAbstractInInterface();
+    }
+
+    @Override
+    public String interfaceStingMethod() {
+        return anonymusMethos.interfaceStingMethod();
+    }
+    
+    @Override
+    public String getName() {
+        return NAME;
+    }
+}
